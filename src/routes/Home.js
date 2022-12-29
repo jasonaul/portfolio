51aa3f7e31e25d520eb41114ef120935a50613bb
jasonaul <jason.aul@gmail.com>
2022-12-28 23:15:06 -0500

    working on making nav mobile-responsive

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const Home = () => {
-  return (
+  return (<>
     <div>Home</div>
+    <div>
+    <button className="btn">Test</button>
+    </div>
+    </>
   )
 }
 
